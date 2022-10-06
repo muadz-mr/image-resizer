47a5ee28fda3bc2820ef69b0a03dabd238046384
Mu'adz Mohd Rosli <mu'adz.rosli@ionnex.com>
2022-10-06 10:57:49 +0800

    feat: create main window and main page

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Using Electron and vanilla HTML, CSS, and JS
 
 - Add "productName": "your app name" in package.json so that on macOS, the app name will show correctly instead of just Electron app
+- "renderer" folder is for frontend assets and files, "main.js" is for backend processes
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: 500,
+        height: 600
+    });
+
+    // mainWindow.loadURL('https://www.twitter.com'); //Can also load url in the browser window
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+app.whenReady().then((resolve) => {
+    createMainWindow();
+}, (reject) => {
+    alert('Oops..something went wrong.');
+});
